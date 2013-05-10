547c893f36c04af0b1043c08584724af41949325
Nick Stewart <nicholas4@gmail.com>
2013-05-10 08:34:13 -0600

    added basic rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seonews::Application.config.secret_token = 'da5c9c847e3009ebbe1c7780b07890d3a2677e9ef83d5297b60d5d5e1ad21294b2e5a0f56af49898c589f631dcb1a255f7d54c95544c865dd2a10a5fdcc29ab5'
